8c79efeb306f9e81ab014ac052c9f78954a86551
Kenrick Halff <kenrick_half8@hotmail.com>
2024-05-06 22:36:47 +0200

    typo

--- src/magic-mayhem.js
+++ src/magic-mayhem.js
@@ -210,7 +210,7 @@ summonsEl.innerHTML += Object.values(SUMMONS_MAP).map(summon => {
 }).join('<br/>');
 
 let socket;
-if (window.location.hostname.includes('localhost') {
+if (window.location.hostname.includes('localhost')) {
   socket = new WebSocket(`ws://${window.location.host}/ws`);
 } else if (window.location.protocol === 'http:') {
   socket = new WebSocket(`http://${window.location.host}/ws`);
